5d79434b1b489a2d350e0ee1f19092980f158403
JoeKe <38954753+JoeKeIsKe@users.noreply.github.com>
2023-04-21 19:44:08 +0800

    Add files via upload

--- src/model/socket.js
+++ src/model/socket.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client'
+import { apiUrl } from '../api'
 
-// const socket = io('https://asteroids.dev.mediasia.cn')
-const socket = io('http://localhost:3001')
+const socket = io('https://asteroids.dev.mediasia.cn')
+// const socket = io('http://localhost:3001')
 
 export default socket
